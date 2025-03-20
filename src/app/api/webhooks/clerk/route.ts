87db038df387e42d8489df695af75d55650c9dc5
Devz0007 <devarshi.h@gmail.com>
2025-03-20 15:23:09 -0700

    Webhookclerkv6

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,4 +1,4 @@
-import { Webhook, WebhookRequiredHeaders } from 'svix';
+import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
 
