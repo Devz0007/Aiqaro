534f6825b007928c2032d0a1ecd4fcf675910f0c
Devz0007 <devarshi.h@gmail.com>
2025-03-20 10:55:44 -0700

    Middlewaretsclerk prod update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+// Define protected routes
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
+  // Protect only specific routes
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
 
 export const config = {
-  publicRoutes: ['/', '/api/(.*)'],
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Always run middleware for API routes
     '/(api|trpc)(.*)',
   ],
 };
