175e3219142a4e399160dac780f149220ead96ec
Devz0007 <devarshi.h@gmail.com>
2025-03-20 18:49:48 -0700

    cu5

--- src/utils/data/user/user-create.ts
+++ src/utils/data/user/user-create.ts
@@ -1,32 +1,76 @@
 // src/utils/data/user/user-create.ts
-import { prisma } from '@/utils/data/client/prima'; // <-- IMPORT the exported client
+'server only';
 
-interface UserCreateProps {
-  user_id: string;
-  email: string;
-  first_name: string;
-  last_name: string;
-  profile_image_url: string;
-}
+import { PostgrestError } from '@supabase/postgrest-js';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { z } from 'zod';
+
+import { userCreateSchema, userUpdateProps } from '@/utils/types/user';
+import { env } from 'data/env/server';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UserCreateResponse = PostgrestError[] | any[];
 
 export const userCreate = async ({
-  user_id,
   email,
   first_name,
   last_name,
   profile_image_url,
-}: UserCreateProps): Promise<void> => {
-  try {
-    await prisma.user.create({
-      data: {
-        email,
-        first_name,
-        last_name,
-        profile_image_url,
+  user_id,
+}: userUpdateProps): Promise<UserCreateResponse> => {
+  const cookieStore = cookies();
+
+  const supabase = createServerClient(
+    env.SUPABASE_URL,
+    env.SUPABASE_SERVICE_KEY,
+    {
+      cookies: {
+        get(name: string) {
+          const cookieValue = cookieStore.get(name)?.value;
+          return cookieValue ?? undefined;
+        },
       },
+    }
+  );
+
+  try {
+    // Validate input data against the schema
+    userCreateSchema.parse({
+      email,
+      first_name,
+      last_name,
+      profile_image_url,
+      user_id,
     });
-  } catch (error) {
+
+    const { data, error } = await supabase
+      .from('user')
+      .insert([
+        {
+          email,
+          first_name,
+          last_name,
+          profile_image_url,
+          user_id,
+        },
+      ])
+      .select();
+
+    if (error) {
+      return [error];
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return data;
+  } catch (error: unknown) {
     console.error('Error creating user:', error);
-    throw error;
+    if (error instanceof z.ZodError) {
+      throw new Error(`Validation Error: ${error.message}`);
+    }
+    if (error instanceof Error && 'message' in error) {
+      throw new Error(error.message);
+    }
+    throw new Error('An unknown error occurred');
   }
 };
--- src/utils/data/user/user-update.ts
+++ src/utils/data/user/user-update.ts
@@ -1,34 +1,73 @@
 // src/utils/data/user/user-update.ts
-import { prisma } from '@/utils/data/client/prima'; // <-- IMPORT the exported client
+'server only';
 
-interface UserUpdateProps {
-  user_id: string;
-  email: string;
-  first_name: string;
-  last_name: string;
-  profile_image_url: string;
-}
+import { PostgrestError } from '@supabase/postgrest-js';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { z } from 'zod';
+
+import { userUpdateSchema, userUpdateProps } from '@/utils/types/user';
+import { env } from 'data/env/server';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UserUpdateResponse = PostgrestError[] | any[];
 
 export const userUpdate = async ({
-  user_id,
   email,
   first_name,
   last_name,
   profile_image_url,
-}: UserUpdateProps): Promise<void> => {
-  try {
-    await prisma.user.update({
-      where: {
-        email, // Use email to find the user
+  user_id,
+}: userUpdateProps): Promise<UserUpdateResponse> => {
+  const cookieStore = cookies();
+
+  const supabase = createServerClient(
+    env.SUPABASE_URL,
+    env.SUPABASE_SERVICE_KEY,
+    {
+      cookies: {
+        get(name: string) {
+          const cookieValue = cookieStore.get(name)?.value;
+          return cookieValue ?? undefined;
+        },
       },
-      data: {
+    }
+  );
+
+  try {
+    // Validate input data against the schema
+    userUpdateSchema.parse({
+      email,
+      first_name,
+      last_name,
+      profile_image_url,
+      user_id,
+    });
+
+    const { data, error } = await supabase
+      .from('user')
+      .update({
         first_name,
         last_name,
         profile_image_url,
-      },
-    });
-  } catch (error) {
+      })
+      .eq('user_id', user_id)
+      .select();
+
+    if (error) {
+      return [error];
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return data;
+  } catch (error: unknown) {
     console.error('Error updating user:', error);
-    throw error;
+    if (error instanceof z.ZodError) {
+      throw new Error(`Validation Error: ${error.message}`);
+    }
+    if (error instanceof Error && 'message' in error) {
+      throw new Error(error.message);
+    }
+    throw new Error('An unknown error occurred');
   }
 };
